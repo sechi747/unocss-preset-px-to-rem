8b8422529057eaf5bdff16649751e331dfa87d27
sechi <sechichen@gmail.com>
2023-02-13 09:27:05 +0800

    Delete dist directory

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { Preset } from 'unocss';
-
-interface PxToRemOptions {
-    baseFontSize?: number;
-    unitPrecision?: number;
-}
-declare function remToPxPreset(options?: PxToRemOptions): Preset;
-
-export { PxToRemOptions, remToPxPreset as default };
